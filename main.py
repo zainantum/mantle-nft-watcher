d08fdb1ac14064e5c168aa0b3c19d4d2947b19a8
Zain <115614618+zainantum@users.noreply.github.com>
2024-01-15 16:45:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# Import Modules
-
 from selenium import webdriver
-# import undetected_chromedriver as uc
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
-import os
-import sys
-import time
+import requests, re, json, os, sys, time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,80 +9,81 @@ from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException
 
 
-found = False
+def get_filter():
+    url = ''
+    resp = requests.get(url)
+    data_filter = resp.json()
+    with open('filter.json', 'w') as json_file:
+        json.dump(data_filter, json_file, indent=2)
 
 
 def run():
+    filter_list = ["Status", "Price", "BACKGROUND", "EYE", "EYEBROW", "HAIR", "HAND", "HEADGEAR", "MOUTH", "OUTFIT", "RARE_HAIR", "SKIN"]
+    data_filter = json.load(open("filter.json"))
+    filter_price = data_filter["price"]
+    filter_trait = data_filter["filter"]
     chrome_options = webdriver.ChromeOptions()
     service = Service(executable_path=ChromeDriverManager().install())
-    # chrome_options.add_argument('--proxy-server=socks5://52.157.88.150:80')
-    # chrome_options.add_argument(f'--proxy-server={propro}')
-    # driver = uc.Chrome(use_subprocess=True, chrome_options=chrome_options)
-    # chrome_options.add_argument('headless')
+    chrome_options.add_argument('headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=chrome_options)
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.maximize_window()
     wait = WebDriverWait(driver, 10)
     try:
         driver.get("https://mintle.app/explore/MANTLE:0x7cf4ac414c94e03ecb2a7d6ea8f79087453caef0")
         time.sleep(5)
-        # input_tag = driver.find_element(By.XPATH, "//input[contains(@type, 'number')]")
+        # close cookie btn
+        cookie_btn = driver.find_element(By.XPATH, "//*[@id='root']/div[2]/button")
+        if cookie_btn:
+            cookie_btn.click()
         price_elm = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//input[contains(@type, 'number')]")))
-        print(len(price_elm))
-        price_elm[1].send_keys("9023")
+        # set lowest price
+        price_elm[0].send_keys(filter_price["low"])
+        # set highest price
+        price_elm[1].send_keys(filter_price["high"])
+        # get all filter elements
         traits_elm = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class, 'sc-iBkjds sc-ftvSup sc-hrZiYQ gYCGXc cLuCCY jCCGsr')]")))
-        print(len(traits_elm))
+        count = 0
         for elm in traits_elm:
-            elm.click()
-            parent_elm = (By.XPATH, "//div[contains(@class, 'sc-bczRLJ sc-gsnTZi IywNy ktCKzB')]")
-            child_elm = (By.XPATH, "//span[contains(@class, 'sc-bjUoiL sc-idiyUo fMQLyS hJGsTm')]")
-            all_filter = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//span[contains(@class, 'sc-bjUoiL sc-idiyUo fMQLyS hJGsTm')]")))
-            for filters in all_filter:
-                print(filters.text)
-            print(all_filter)
-            time.sleep(5)
-        time.sleep(10)
-        # get element
-        # element = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[3]/div[3]/div[1]/button[2]")
-        # element.click()
-        # pw = driver.find_element(By.XPATH, "//input[@name='wallet_password']")
-        # con_pw = driver.find_element(By.XPATH, "//input[@name='wallet_confirm_password']")
-        # pw.send_keys("kopisaja")
-        # con_pw.send_keys("kopisaja")
-        # create = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[3]/div[4]/div[3]/button[1]")
-        # create.click()
-        # try:
-        #     element = WebDriverWait(driver, 120).until(
-        #         EC.presence_of_element_located((By.CLASS_NAME, "reapop__notification-message"))
-        #     ).text
-        #     print(element)
-        #     if element == "Unable to reach faucet":
-        #         driver.quit()
-        #     else:
-        #         print("faucet ready")
-        #         time.sleep(7)
-        #         sendDrop = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[8]/div[1]/div[1]/button[1]")
-        #         sendDrop.click()
-        #         time.sleep(2)
-        #         send = driver.find_element(By.XPATH,
-        #                                    "/html/body/div[1]/div[1]/div[2]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[3]/a[1]")
-        #         send.click()
-        #         time.sleep(2)
-        #         address = driver.find_element(By.XPATH, "//input[@name='toAddress']")
-        #         jml = driver.find_element(By.XPATH, "//input[@name='amount']")
-        #         address.send_keys("")
-        #         jml.send_keys("9.99")
-        #         sendAct = driver.find_element(By.XPATH,
-        #                                       "/html/body/div[1]/div[1]/div[2]/div[8]/div[1]/div[1]/div[1]/div[1]/div[3]/button[1]")
-        #         sendAct.click()
-        #         time.sleep(15)
-        #         driver.quit()
-        # finally:
-        #     print("done")
-    except WebDriverException:
+            if count > 1 and filter_list[count] in filter_trait:
+                print(filter_trait[filter_list[count]])
+                filter = filter_list[count]+"-filter"
+                filter_scroll = filter_list[count-1]+"-filter"
+                print(filter_scroll)
+                # scroll element to last filter
+                if filter_scroll != "HAND-filter" and filter_scroll != "OUTFIT-filter":
+                    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.XPATH, "//div[@data-testid='"+filter_scroll+"']"))
+                elm.click()
+                time.sleep(1)
+                child_elm = driver.find_elements(By.XPATH, "//div[@data-testid='"+filter+"']/div/div/div/div[*]/button/div/span/div/span")
+                for c in child_elm:
+                    if c.text.lower() in filter_trait[filter_list[count]]:
+                        driver.execute_script("arguments[0].scrollIntoView();", c)
+                        driver.execute_script("return arguments[0].parentNode.parentNode.parentNode.parentNode.click();", c)
+                
+                time.sleep(2)
+            
+            count+=1
+        
+        # apply all filter
+        apply_elm = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div/div[3]/div/div[1]/button")
+        driver.execute_script("arguments[0].scrollIntoView();", apply_elm)
+        apply_elm.click()
+        # start scraping all nft
+        time.sleep(2)
+        all_nft = driver.find_elements(By.XPATH, "//span[@data-anchor='item-title']")
+        for nft in all_nft:
+            children = driver.execute_script("return arguments[0].parentNode.parentNode.parentNode.nextElementSibling.querySelector('span.sc-bjUoiL.sc-idiyUo.fMQLyS.hJGsTm').textContent;", nft)
+            print("Name: ",nft.text,children)
+            if nft == all_nft[-1]:
+                driver.execute_script("arguments[0].scrollIntoView();", nft)
+        
+        time.sleep(5)
+        driver.quit()
+    except WebDriverException as e:
         driver.quit()
 
 
+get_filter()
 run()
-
