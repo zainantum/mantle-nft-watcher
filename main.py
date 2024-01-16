68e5539b044bc5fe7a718e96af8ae459b338cd1d
Zain <115614618+zainantum@users.noreply.github.com>
2024-01-16 08:03:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,47 @@ def get_filter():
     url = ''
     resp = requests.get(url)
     data_filter = resp.json()
+    print(data_filter)
     with open('filter.json', 'w') as json_file:
         json.dump(data_filter, json_file, indent=2)
 
 
+def get_all_nft(driver, callback):
+    try:
+        time.sleep(5)
+        count = 0
+        if callback:
+            all_nft = callback.find_elements(By.XPATH, "//following-sibling::span[position() <= 3][@data-anchor='item-title']")
+        else:
+            all_nft = driver.find_elements(By.XPATH, "//span[@data-anchor='item-title']")
+        print(len(all_nft))
+        for nft in all_nft:
+            children = driver.execute_script(
+                "return arguments[0].parentNode.parentNode.parentNode.nextElementSibling.querySelector('span.sc-bjUoiL.sc-idiyUo.fMQLyS.hJGsTm').textContent;",
+                nft)
+            print("Name: ", nft.text, children)
+            if count % 4 == 0:
+                driver.execute_script("arguments[0].scrollIntoView();", nft)
+                time.sleep(2)
+            if nft == all_nft[-1]:
+                get_all_nft(driver, callback)
+
+            count += 1
+    except WebDriverException as e:
+        print(e)
+        driver.quit()
+
+
 def run():
-    filter_list = ["Status", "Price", "BACKGROUND", "EYE", "EYEBROW", "HAIR", "HAND", "HEADGEAR", "MOUTH", "OUTFIT", "RARE_HAIR", "SKIN"]
+    filter_list = ["Status", "Price", "BACKGROUND", "EYE", "EYEBROW", "HAIR", "HAND", "HEADGEAR", "MOUTH", "OUTFIT",
+                   "RARE_HAIR", "SKIN"]
     data_filter = json.load(open("filter.json"))
     filter_price = data_filter["price"]
     filter_trait = data_filter["filter"]
+    print(filter_trait)
     chrome_options = webdriver.ChromeOptions()
     service = Service(executable_path=ChromeDriverManager().install())
-    chrome_options.add_argument('headless')
+    # chrome_options.add_argument('headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -39,46 +68,48 @@ def run():
             cookie_btn.click()
         price_elm = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//input[contains(@type, 'number')]")))
         # set lowest price
-        price_elm[0].send_keys(filter_price["low"])
+        price_elm[0].send_keys(filter_price["lowest"])
         # set highest price
-        price_elm[1].send_keys(filter_price["high"])
+        price_elm[1].send_keys(filter_price["highest"])
         # get all filter elements
-        traits_elm = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class, 'sc-iBkjds sc-ftvSup sc-hrZiYQ gYCGXc cLuCCY jCCGsr')]")))
+        traits_elm = wait.until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//button[contains(@class, 'sc-iBkjds sc-ftvSup sc-hrZiYQ gYCGXc cLuCCY jCCGsr')]")))
         count = 0
         for elm in traits_elm:
             if count > 1 and filter_list[count] in filter_trait:
-                print(filter_trait[filter_list[count]])
-                filter = filter_list[count]+"-filter"
-                filter_scroll = filter_list[count-1]+"-filter"
+                print(filter_list[count], filter_trait[filter_list[count]])
+                filter = filter_list[count] + "-filter"
+                filter_scroll = filter_list[count - 1] + "-filter"
                 print(filter_scroll)
                 # scroll element to last filter
                 if filter_scroll != "HAND-filter" and filter_scroll != "OUTFIT-filter":
-                    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.XPATH, "//div[@data-testid='"+filter_scroll+"']"))
+                    driver.execute_script("arguments[0].scrollIntoView();",
+                                          driver.find_element(By.XPATH, "//div[@data-testid='" + filter_scroll + "']"))
                 elm.click()
                 time.sleep(1)
-                child_elm = driver.find_elements(By.XPATH, "//div[@data-testid='"+filter+"']/div/div/div/div[*]/button/div/span/div/span")
+                child_elm = driver.find_elements(By.XPATH,
+                                                 "//div[@data-testid='" + filter + "']/div/div/div/div[*]/button/div/span/div/span")
                 for c in child_elm:
-                    if c.text.lower() in filter_trait[filter_list[count]]:
+                    if c.text in filter_trait[filter_list[count]]:
                         driver.execute_script("arguments[0].scrollIntoView();", c)
-                        driver.execute_script("return arguments[0].parentNode.parentNode.parentNode.parentNode.click();", c)
-                
+                        driver.execute_script(
+                            "return arguments[0].parentNode.parentNode.parentNode.parentNode.click();", c)
+
+                    if c.text not in filter_trait[filter_list[count]] and c == child_elm[-1]:
+                        driver.execute_script("arguments[0].scrollIntoView();", c)
                 time.sleep(2)
-            
-            count+=1
-        
+
+            count += 1
+
         # apply all filter
-        apply_elm = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div/div[3]/div/div[1]/button")
+        apply_elm = driver.find_element(By.XPATH,
+                                        "//*[@id='root']/div/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div/div[3]/div/div[1]/button")
         driver.execute_script("arguments[0].scrollIntoView();", apply_elm)
         apply_elm.click()
         # start scraping all nft
         time.sleep(2)
-        all_nft = driver.find_elements(By.XPATH, "//span[@data-anchor='item-title']")
-        for nft in all_nft:
-            children = driver.execute_script("return arguments[0].parentNode.parentNode.parentNode.nextElementSibling.querySelector('span.sc-bjUoiL.sc-idiyUo.fMQLyS.hJGsTm').textContent;", nft)
-            print("Name: ",nft.text,children)
-            if nft == all_nft[-1]:
-                driver.execute_script("arguments[0].scrollIntoView();", nft)
-        
+        get_all_nft(driver, "")
+
         time.sleep(5)
         driver.quit()
     except WebDriverException as e:
